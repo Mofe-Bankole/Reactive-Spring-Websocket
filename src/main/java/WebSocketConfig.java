2f7b7902161a4ab0259053c8af225b15886acc37
Mofe-banks01 <bankoleadedotun16@gmail.com>
2025-02-24 11:20:36 +0100

    Couldnt code for discreet reasone< now im back

--- src/main/java/WebSocketConfig.java
+++ src/main/java/WebSocketConfig.java
@@ -3,9 +3,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.HandlerMapping;
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketSession;
 import org.springframework.web.reactive.socket.server.WebSocketService;
 import org.springframework.web.reactive.socket.server.support.HandshakeWebSocketService;
 import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
+import reactor.core.publisher.Mono;
 
 import java.util.Collections;
 import java.util.logging.Logger;
@@ -16,28 +19,35 @@ import java.util.logging.Logger;
 public class WebSocketConfig {
 
     @Bean
-    public Logger logger() {
-        return Logger.getLogger
-                (WebSocketConfig.class.getName());
+    public Logger logger (){
+        return Logger.getLogger(WebSocketConfig.class.getName());
     }
 
     @Bean
     public HandlerMapping webSocketMapping(WebSocketHandler webSocketHandler){
         return new SimpleUrlHandlerMapping
-                (Collections.singletonMap("/websocket" , webSocketHandler) , 1);
+                (Collections.singletonMap("/ws" , webSocketHandler) , 1);
     }
 
+    //ASH protocol
     @Bean
     public WebSocketHandlerAdapter socketHandlerAdapter(){
         return new WebSocketHandlerAdapter(webSocketService());
     }
 
-    private WebSocketService webSocketService() {
+    private WebSocketService webSocketService(){
         return new HandshakeWebSocketService();
     }
 
     @Bean
     public WebSocketHandler webSocketHandler(Logger logger){
-        return new WebSocketHandler(logger);
+        return new WebSocketHandler() {
+            @Override
+            public Mono<Void> handle(WebSocketSession session) {
+                return session.send(
+                        session.receive().map(
+                                msg -> (session.textMessage(msg.getPayloadAsText()))));
+            }
+        };
     }
 }

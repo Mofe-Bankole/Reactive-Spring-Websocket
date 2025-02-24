6547cc23b495b351bec41c943e3f7888154fe380
Mofe-banks01 <bankoleadedotun16@gmail.com>
2025-02-24 11:20:03 +0100

    Couldnt code for discreet reasone< now im back

--- src/main/java/WebSocketHandler.java
+++ src/main/java/WebSocketHandler.java
@@ -1,25 +1,25 @@
 import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.WebSocketSession;
 import reactor.core.publisher.Mono;
 
 import java.util.logging.Logger;
 
 public class WebSocketHandler implements org.springframework.web.reactive.socket.WebSocketHandler {
     private final Logger logger;
-
+            
     public WebSocketHandler(Logger logger) {
         this.logger = logger;
     }
 
     @Override
-    public Mono<Void> handle(org.springframework.web.reactive.socket.WebSocketSession session) {
+    public Mono<Void> handle(WebSocketSession session) {
         return session.receive()
                 .map(WebSocketMessage::getPayloadAsText)
                 .doOnNext(message -> {
-                    logger
-                            .info("Received Message " + message);
-                    session.send(Mono.just(session.textMessage("Echo " + message))).subscribe();
+                    logger.info("Received Message " + message);
+                    session.send(Mono.just
+                            (session.textMessage("Echo " + message))).subscribe();
                 })
                 .then();
     }
-
 }
--- src/main/java/com/devmofe/Reactive/Spring/Websocket/ReactiveSpringWebsocketApplication.java
+++ src/main/java/com/devmofe/Reactive/Spring/Websocket/ReactiveSpringWebsocketApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReactiveSpringWebsocketApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveSpringWebsocketApplication.class, args);
 	}
-
 }
